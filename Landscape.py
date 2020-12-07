997f4c3dae09a31de0eda1e5b469a9d4695991b9
Никита Матюхин <matiukhin.nd@phystech.ed>
2020-12-07 20:04:08 +0300

    Тест

---
+++ Landscape.py
@@ -0,0 +1,8 @@
+
+root = tk.Tk()
+fr = tk.Frame(root)
+length = 800
+height = 600
+root.geometry(str(length) + 'x' + str(height))
+canv = tk.Canvas(root, bg='white')
+canv.pack(fill=tk.BOTH, expand=1)
--- Main.py
+++ Main.py
@@ -1,12 +1,6 @@
 import tkinter as tk
 
-root = tk.Tk()
-fr = tk.Frame(root)
-length = 800
-height = 600
-root.geometry(str(length) + 'x' + str(height))
-canv = tk.Canvas(root, bg='white')
-canv.pack(fill=tk.BOTH, expand=1)
+
 
 
 from Animals.py import *
