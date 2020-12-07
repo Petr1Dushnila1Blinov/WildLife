1e55d1ffbfe5d3dda6e8b804c5f7880e5b6e0c31
kuznets26 <kuznetsov.gs@phystech.edu>
2020-12-07 19:55:16 +0300

    Started developing

--- Animals.py
+++ Animals.py
@@ -10,8 +10,9 @@ class Cattle:
         self.health = 100  # represents the health points
         self.anxiety = 0  # represents how anxious the animal is
         self.mass = 10**3  # mass of the animal
-        self.radius = 1
+        self.radius = 1  # radius of image
         self.color = 'green'
+        # creates the image of an animal
         self.id = canv.create_oval(self.coord_x - self.radius,
                                     self.coord_y - self.radius,
                                     self.coord_x + self.radius,
@@ -19,10 +20,40 @@ class Cattle:
                                     fill=self.color)
 
     def move(self):
-        canv.coords(
-            self.id,
-            self.x - self.r + self.velocity_x,
-            self.y - self.r + self.velocity_y,
-            self.x + self.r + self.velocity_x,
-            self.y + self.r + self.velocity_y)
+        canv.coords(self.id,
+                    self.coord_x - self.radius + self.velocity_x,
+                    self.coord_y - self.radius + self.velocity_y,
+                    self.coord_x + self.radius + self.velocity_x,
+                    self.coord_y + self.radius + self.velocity_y)
+
+    def death(self):
+        if self.health == 0:
+            canv.delete(self.id)
+
+class Predator:
+    def __init__(self):
+        self.coord_x = 1  # x coordinate
+        self.coord_y = 1  # y coordinate
+        self.velocity_x = 2  # speed x-axis
+        self.velocity_y = 1  # speed y-axis
+        self.hunger = 0  # represents how hungry the animal is
+        self.thirst = 0  # represents how thirsty the animal is
+        self.health = 100  # represents the health points
+        self.mass = 10**3  # mass of the animal
+        self.radius = 1  # radius of image
+        self.color = 'red'
+        # creates the image of an animal
+        self.id = canv.create_oval(self.coord_x - self.radius,
+                                    self.coord_y - self.radius,
+                                    self.coord_x + self.radius,
+                                    self.coord_y + self.radius,
+                                    fill=self.color)
+
+    def move(self):
+        canv.coords(self.id,
+                    self.coord_x - self.radius + self.velocity_x,
+                    self.coord_y - self.radius + self.velocity_y,
+                    self.coord_x + self.radius + self.velocity_x,
+                    self.coord_y + self.radius + self.velocity_y)
+
 
--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ import tkinter as tk
 
 root = tk.Tk()
 fr = tk.Frame(root)
-root.geometry('800x600')
+length = 800
+height = 600
+root.geometry(str(length) + 'x' + str(height))
 canv = tk.Canvas(root, bg='white')
 canv.pack(fill=tk.BOTH, expand=1)
 
