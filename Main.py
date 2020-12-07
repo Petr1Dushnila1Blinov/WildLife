7116e9ed75b780dc31f181caa2deeba2b994572e
kuznets26 <kuznetsov.gs@phystech.edu>
2020-12-07 23:41:38 +0300

    A bit of refactoring

--- Main.py
+++ Main.py
@@ -8,6 +8,10 @@ predator_1 = Predator()
 cattle = [cattle_1]
 predators = [predator_1]
 
+
+def lake_force(obj):
+
+    obj.coord_x =
 def main_game():
     for p in predators:
         for c in cattle:
