d05eed71ca08309704afd072a66910a1905f82e1
Никита Матюхин <matiukhin.nd@phystech.ed>
2020-12-07 20:17:16 +0300

    Трава

--- Landscape.py
+++ Landscape.py
@@ -1,8 +1,15 @@
+from random import randrange as rnd, choice
+import tkinter as tk
+import math
+import time
 
 root = tk.Tk()
 fr = tk.Frame(root)
 length = 800
 height = 600
 root.geometry(str(length) + 'x' + str(height))
-canv = tk.Canvas(root, bg='white')
+canv = tk.Canvas(root, bg='lime green')
 canv.pack(fill=tk.BOTH, expand=1)
+
+
+tk.mainloop()
